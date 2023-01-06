a4177477a1d18a804244e22e8b78dec175258faf
Varsha-Patil7190 <121961658+Varsha-Patil7190@users.noreply.github.com>
2023-01-06 20:14:30 +0900

    commit
    hii team
    review my code and give me approval as per code.
    thank you

---
+++ StartDerive/src/com/Simple/FirstHello.java
@@ -0,0 +1,13 @@
+package com.Simple;
+
+public class FirstHello {
+
+	public static void main(String[] args) {
+		System.out.println("Hiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii");
+		System.out.println("Helloooo");
+		System.out.println("GoodMornning");
+		System.out.println("Varsha Patil");
+		System.out.println("welcome in github");
+	}
+
+}
